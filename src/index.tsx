0e64fd77dd065b7a6eb318dd9db742decc70d574
igor poughtach <ingvardm@gmail.com>
2022-06-11 15:29:53 +0300

    rebrend moar

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import './reset.css'
 import './index.css'
 
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
@@ -9,8 +8,4 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
